154ee28c6152a60853a3c499583b4726aa30080e
Guillaume Viguier <guillaume@viguierjust.com>
2015-01-26 11:37:36 +0100

    Added filter by direct parent in the API

--- plugins/restful/taxonomy_term/hr_location/1.0/RestfulEntityTaxonomyTermLocations.class.php
+++ plugins/restful/taxonomy_term/hr_location/1.0/RestfulEntityTaxonomyTermLocations.class.php
@@ -8,6 +8,27 @@
 class RestfulEntityTaxonomyTermLocations extends \RestfulEntityBaseTaxonomyTerm {
 
   /**
+   * Overrides RestfulEntityBaseNode::addExtraInfoToQuery()
+   * 
+   * Adds proper query tags
+   */
+  protected function addExtraInfoToQuery($query) {
+    parent::addExtraInfoToQuery($query);
+    $filters = $this->parseRequestForListFilter();
+    if (!empty($filters)) {
+      $addTag = FALSE;
+      foreach ($filters as $filter) {
+        if ($filter['public_field'] == 'parent') {
+          $addTag = TRUE;
+        }
+      }
+      if ($addTag == TRUE) {
+        $query->addTag('hr_locations_filter_parent');
+      }
+    }
+  }
+
+  /**
    * Overrides \RestfulEntityBase::publicFieldsInfo().
    */
   public function publicFieldsInfo() {
@@ -22,14 +43,47 @@ class RestfulEntityTaxonomyTermLocations extends \RestfulEntityBaseTaxonomyTerm 
     );
 
     $public_fields['admin_level'] = array(
-      'callback' => array($this, 'getAdminLevel'),
+      'property' => 'field_loc_admin_level',
     );
 
     $public_fields['geolocation'] = array(
       'callback' => array($this, 'getGeolocation'),
     );
 
+    $public_fields['parent'] = array(
+      'property' => 'parent',
+      'resource' => array(
+        'hr_location' => 'locations',
+      ),
+      'process_callbacks' => array(array($this, 'getEntity')),
+    );
+
     return $public_fields;
+  }
+
+  protected function getEntity($wrapper) {
+    $single = FALSE;
+    foreach ($wrapper as $id => &$item) {
+      if (is_string($item) || $single == TRUE) {
+        if ($single == FALSE) {
+          $single = TRUE;
+        }
+        if (!in_array($id, array('id', 'label', 'self'))) {
+          unset($wrapper[$id]);
+        }
+      }
+      else {
+        $array_item = (array)$item;
+        $properties = array_keys($array_item);
+        foreach ($properties as $property) {
+          if (!in_array($property, array('id', 'label', 'self'))) {
+            unset($array_item[$property]);
+          }
+        }
+        $item = (object)$array_item;
+      }
+    }
+    return $wrapper;
   }
 
   protected function getParents($wrapper) {
